61998e1dbba0e56936c7574221449e81c95566e4
Teetuch6313131 <78196842+Teetuch6313131@users.noreply.github.com>
2021-11-10 06:46:52 +0000

    f

--- Main.java
+++ Main.java
@@ -1,4 +1,5 @@
 import java.util.*;
+import java.util.concurrent.*;
 import java.io.File;
 import java.util.ArrayList;
 
@@ -45,6 +46,9 @@ class Main {
                 else{System.out.printf(",");}
               }
             }
+            for(int i = 0; i< factList.size(); i++){
+              factList.get(i).setThread(factList.size());
+            }
             //End of get Factory
             scan.close();
             check = 0;
@@ -61,18 +65,33 @@ class Main {
     System.out.printf("\nThread %-7s >>",Thread.currentThread().getName());
     System.out.printf(" Enter number of days = ");
     dayMax = input.nextInt();
+
+    CyclicBarrier finish = new CyclicBarrier(factList.size());
+
+    /*for(int i = 0; i< factList.size(); i++){
+      factList.get(i).setCyclicBarrier(finish);
+    }*/
+
+    Buffer b = new Buffer(factList.size());
+    for(int i = 0; i< factList.size(); i++){
+      factList.get(i).setBuffer(b);
+    }
+
     for(int day = 1; day <= dayMax; day++){
       System.out.printf("\n\nThread %-7s >> Day %3d\n",Thread.currentThread().getName(),day);
       for(int i = 0; i < matList.size(); i++){
         matList.get(i).put(matPerDay);
         System.out.printf("Thread %-7s >> Put %5d %s   balance = %5d %s\n",Thread.currentThread().getName(),matPerDay,matList.get(i).getName(),matList.get(i).getBalance(),matList.get(i).getName());
       }
+      System.out.println("");
       for(int i = 0; i < factList.size(); i++){
         factList.get(i).updateMatList(matList);
       }
+      ///////////////////////////////////////////////
       for(int i = 0; i < factList.size(); i++){
-        factList.get(i).run();
+        factList.get(i).start();
       }
+      /////////////////////////////////////////////////
     }
   }
 }
@@ -85,6 +104,7 @@ class OneShareMaterial{
     name=n;
     Balance=0;
   }
+
   synchronized public void put(int x)
   {
     Balance+=x;
@@ -92,7 +112,7 @@ class OneShareMaterial{
   synchronized public int get(int y)
   {
     int temp;
-    if(y>=Balance)
+    if(y<=Balance)
     {
       Balance-=y;
       return y;
@@ -108,35 +128,80 @@ class OneShareMaterial{
   public int getBalance(){return Balance;}
 }
 
+class Buffer{
+  private int numOfThread;
+
+  public Buffer(int n){numOfThread = n;}
+
+  synchronized public void barrier(){
+    Factory th = (Factory)(Thread.currentThread());
+    int share = th.getID();
+    while(share < th.howManyThread()){
+      try { wait(); } catch(Exception e) {}
+    }
+    notifyAll();
+  }
+}
+
 class Factory extends Thread{
-  private int id, lotSize;
+  private int id, lotSize, threadNumber, day, complete;
   private String product;
   private ArrayList<Integer> material = new ArrayList<Integer>();
   private ArrayList<OneShareMaterial> materialList = new ArrayList<OneShareMaterial>();
-  private boolean on;
+  private Buffer buffer;
+  private ArrayList<Integer> matInstock = new ArrayList<Integer>();
+  protected CyclicBarrier cfinish;
 
+  public Factory(){}
   public Factory(int x, String s, int y, ArrayList<Integer> arr){
     id = x;
     product = s;
     this.setName(s);
     lotSize = y;
     material = arr;
+    for(int i = 0; i < arr.size(); i++){
+      matInstock.add(0);
+    }
+    complete = 0;
+    day = 1;
   }
+
+  public void setBuffer(Buffer b){buffer = b;}
+  public void setCyclicBarrier(CyclicBarrier x){cfinish = x;}
+  public int getID(){return id;}
+  public void setThread(int x){threadNumber = x;}
+  public int howManyThread(){return threadNumber;}
 
   public void updateMatList(ArrayList<OneShareMaterial> x){materialList = x;}
 
   private void getMat(){
     for(int i = 0; i < materialList.size(); i++){
-      int mat = materialList.get(i).get(material.get(i));
-      System.out.printf("Thread %-7s >> Get %5d %s   balance = %5d %s\n",this.getName(),mat,materialList.get(i).getName(),materialList.get(i).getBalance(),materialList.get(i).getName());
+      int mat = materialList.get(i).get(material.get(i)-matInstock.get(i));
+      if(mat > 0){
+        System.out.printf("Thread %-7s >> Get %5d %s   balance = %5d %s\n",this.getName(),mat,materialList.get(i).getName(),materialList.get(i).getBalance(),materialList.get(i).getName());
+        matInstock.set(i, matInstock.get(i)+mat);
+      }
     }
+
+    for(int i = 0; i < materialList.size(); i++){
+      if(material.get(i)!=matInstock.get(i)){
+        System.out.printf("Thread %-7s >> -----Fail\n",this.getName());
+        return;
+      }
+    }
+    complete += 1;
+    System.out.printf("Thread %-7s >> +++++Complete Lot %2d\n",this.getName(), complete);
+
+    for(int i = 0; i < matInstock.size(); i++){
+      matInstock.set(i, 0);
+    }
+    return;
   }
 
   public void run(){
-    on = true;
-    while(on){
-      getMat();
-      on = false;
-    }
+    //try{cfinish.await();}catch(Exception e){}
+    buffer.barrier();
+    getMat();
+
   }
 }
